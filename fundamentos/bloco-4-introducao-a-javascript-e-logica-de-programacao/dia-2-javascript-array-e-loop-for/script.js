5de1dd38417301fba66785d2579eda71a3a2681b
Yuri Vasconcellos <y.vasconcellos@outlook.com>
2022-03-08 18:29:54 -0300

    exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,8 +1,11 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 22];
 let soma = 0;
+let mediaAritmetica = 0
 
 for (let index = 0; index<numbers.length; index += 1) {
     console.log(numbers[index]);
     soma = (numbers[index] + soma);
+    mediaAritmetica = soma/(index+1);
 }
-console.log(soma)
+console.log("a soma dos elementos é: ",soma);
+console.log("a média dos elementos é: ",mediaAritmetica);
